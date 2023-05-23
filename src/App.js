b1c02da3359d1304f6bb05a3cb76cabd65ce8bf6
sayli01 <ranesayli1929@gmail.com>
2023-05-23 06:53:25 +0530

    user defined tag

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-//Code Formatting
-//Auto Save /On Save
-//Auto Suggestion
-//Run the program :: npm start-done only once (there is no live server extension)
-
-//JSX ::() -written in parenthesis
-//    :: parenthesis is optional if ur html is one liner
-//    :: parenthesis is required for multi-line html
-//    :: rrquired with two tags
+//React function
+//SPL function :: understood by REACT
+//return JSX (means its a react function)
+//App tag is default
 
 function App() {
   return (
     <div>
-      {" "}
-      <div>
-        <h1>Helloooooo</h1>
-      </div>
-      <div>Mumbai</div>
+      <h1>"!!! Hello World !!!"</h1>
+      <hr />
+      <h1>"Project Title"</h1>
+      <Header />
+      <hr />
+      <h1>"Project Title"</h1>
+      <Header />
+      <hr />
+      <h1>"Project Title"</h1>
+      <Header />
+      <hr />
+      <h1>"Project Title"</h1>
+      <Header />
+      <hr />
+    </div>
+  );
+}
+
+//User Defined Tag
+//<Header />
+//re-usable block of code using header tag
+function Header() {
+  return (
+    <div>
+      Lorem ipsum is placeholder text commonly used in the graphic, print, and
+      publishing industries for previewing layouts and visual mockups.Lorem
+      ipsum is placeholder text commonly used in the graphic, print, and
+      publishing industries for previewing layouts and visual mockups.
     </div>
   );
 }
