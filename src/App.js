8c851cdbcfc68f048c8bd21efe48b1e1af15d730
sayli01 <ranesayli1929@gmail.com>
2023-05-23 07:02:31 +0530

    user defined tags

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
 //React function
 //SPL function :: understood by REACT
 //return JSX (means its a react function)
-//App tag is default
+//App tag is default-others can be called inside app()
 
 function App() {
   return (
     <div>
-      <h1>"!!! Hello World !!!"</h1>
-      <hr />
-      <h1>"Project Title"</h1>
       <Header />
-      <hr />
-      <h1>"Project Title"</h1>
-      <Header />
-      <hr />
-      <h1>"Project Title"</h1>
-      <Header />
-      <hr />
-      <h1>"Project Title"</h1>
-      <Header />
-      <hr />
+      <MainBody />
+      <MainBody />
+      <MainBody />
+      <MainBody />
+      <MainBody />
+      <MainBody />
+      <MainBody />
+      <MainBody />
+      <Footer />
     </div>
   );
 }
 
-//User Defined Tag
-//<Header />
 //re-usable block of code using header tag
 function Header() {
+  return <h1>Hello Welcome </h1>;
+}
+
+function MainBody() {
   return (
     <div>
       Lorem ipsum is placeholder text commonly used in the graphic, print, and
       publishing industries for previewing layouts and visual mockups.Lorem
       ipsum is placeholder text commonly used in the graphic, print, and
       publishing industries for previewing layouts and visual mockups.
+      <br />.
+      <br />
     </div>
   );
 }
 
+function Footer() {
+  return (
+    <div>
+      <h1>This is Footer</h1>
+    </div>
+  );
+}
 export default App;
