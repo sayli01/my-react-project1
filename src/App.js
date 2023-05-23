1bcaeeafbaf0c6dd2aa2549a061c57aecdd830af
sayli01 <ranesayli1929@gmail.com>
2023-05-23 06:32:50 +0530

    jsx-importance of parent tag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-//JSX +> Javascript and XML
-//Predefined tag :: <h1>,<p>,<div> (predefined tag)
-//User Defined tag :: <App /> / <App></App>
-
 //XML - User defined tag
 //X :: Extended Markup Language
+//in xml, html is flexible , but xml is not flexible
+//XML :: STRICT-(complex than html)
+//XML :: there must be one root tag i.e parent
+
+//code formatting
+//Auto Save
+//Auto suggestion
+//Run the program :: nopm start (there is no live server extension)
 
 function App() {
-  return <h1>"!!! Hello World !!!"</h1>;
+  return (
+    <div>
+      <h1>"!!! Hello World !!!"</h1>
+      <h1>"!!! Welcome !!!"</h1>
+    </div>
+  );
 }
 
 export default App;
