1c394d585c105fa6fa4ea8dc66e6934521ce3959
sayli01 <ranesayli1929@gmail.com>
2023-05-23 07:24:49 +0530

    commit

---
+++ src/pages/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <h1>Is this page useful ?</h1>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/pages/MainBody.js
@@ -0,0 +1,14 @@
+function MainBody() {
+  return (
+    <div>
+      Lorem ipsum is placeholder text commonly used in the graphic, print, and
+      publishing industries for previewing layouts and visual mockups.Lorem
+      ipsum is placeholder text commonly used in the graphic, print, and
+      publishing industries for previewing layouts and visual mockups.
+      <br />.
+      <br />
+    </div>
+  );
+}
+
+export default MainBody;
